db0aa134e491c20477f80a6cbdf52db63469b455
Grant Coles <grantwc94@gmail.com>
2019-05-23 12:55:20 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "Bow ya shits";
+  botResponse = "BOW YA SHITS";
 
   options = {
     hostname: 'api.groupme.com',
