56609c044f961cdd27f6f3335a0079f83c8a3b32
Grant Coles <grantwc94@gmail.com>
2019-06-04 10:06:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /(BOBBY B)/i;
-
+  console.log(request);
   if(request.text && botRegex.test(request.text)) {
     sleep(2000)
     this.res.writeHead(200);
